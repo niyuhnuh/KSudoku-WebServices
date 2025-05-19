dd9a596221efcee8f3b530f3dce961647ae02e76
niyuhnuh <nianiaandrea@gmail.com>
2025-05-19 23:49:35 +0800

    feat(api): add GameLogicControllers class

---
+++ src/main/java/com/ksuduko/controllers/GameLogicControllers.java
@@ -0,0 +1,18 @@
+package com.ksuduko.controllers;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameLogicControllers {
+	
+	@GetMapping("/getGameSolution")
+	public Map<String, Map<String, String>> gameMap(){
+		Map<String, Map<String, String>> map = null;
+		return map;
+	}
+	
+	
+}
