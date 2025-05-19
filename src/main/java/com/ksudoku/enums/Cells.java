71edabb4618702be0fb0dd83526f8a5d61f702ca
niyuhnuh <nianiaandrea@gmail.com>
2025-05-19 23:56:31 +0800

    component(board): add cells and board

---
+++ src/main/java/com/ksudoku/enums/Cells.java
@@ -0,0 +1,41 @@
+package com.ksudoku.enums;
+
+public enum Cells {
+	CELL_1("C1"),
+	CELL_2("C1"),
+	CELL_3("C1"),
+	CELL_4("C1"),
+	CELL_5("C1"),
+	CELL_6("C1"),
+	CELL_7("C1"),
+	CELL_8("C1"),
+	CELL_9("C1"),
+	CELL_10("C1"),
+	CELL_11("C1"),
+	CELL_12("C1"),
+	CELL_13("C1"),
+	CELL_14("C1"),
+	CELL_15("C1"),
+	CELL_16("C1"),
+	CELL_17("C1"),
+	CELL_18("C1"),
+	CELL_19("C1"),
+	CELL_20("C1"),
+	CELL_21("C1"),
+	CELL_22("C1"),
+	CELL_23("C1"),
+	CELL_24("C1"),
+	CELL_25("C1"),
+	CELL_26("C1"),
+	CELL_27("C1"),
+	CELL_28("C1"),
+	CELL_29("C1"),
+	CELL_30("C1"),
+	CELL_31("C1"),
+
+	;
+
+	Cells(String string) {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/com/ksudoku/objects/Board.java
@@ -0,0 +1,5 @@
+package com.ksudoku.objects;
+
+public class Board {
+
+}
